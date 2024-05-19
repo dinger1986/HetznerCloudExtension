e31cc5d549cfc0a811bd45551759ab8b38607b10
dinger1986 <daniel.lamb@flonix.co.uk>
2024-05-19 11:03:59 +0100

    Update routes.php
    Signed-off-by: dinger1986 <daniel.lamb@flonix.co.uk>

--- routes.php
+++ routes.php
@@ -1,4 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
+
 Route::post('/hetzner/status/{product}', [App\Extensions\Servers\HetznerCloud\HetznerCloud::class, 'status'])->name('extensions.hetzner.status');
 Route::post('/hetzner/revdns/{product}', [App\Extensions\Servers\HetznerCloud\HetznerCloud::class, 'revdns'])->name('extensions.hetzner.revdns');
